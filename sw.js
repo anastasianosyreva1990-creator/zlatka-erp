0f7afc333d3f8ffab43e74340594a5a15d4a651c
anastasianosyreva1990-creator <anastasia.nosyreva1990@gmail.com>
2026-05-24 09:22:51 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'zlatka-v1';
+const ASSETS = ['/zlatka-erp/', '/zlatka-erp/index.html'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
